597aaa9f6ed586161b891b1c9c5229994bc75176
Max <agnusmaximus@berkeley.edu>
2016-10-03 21:02:16 -0700

    Update

--- inception/inception/sync_replicas_optimizer_summarized.py
+++ inception/inception/sync_replicas_optimizer_summarized.py
@@ -409,12 +409,12 @@ class SyncReplicasOptimizerSummarized(optimizer.Optimizer):
       real_grad = lambda: [control_flow_ops.group(*train_ops)]
       final_train_ops = control_flow_ops.cond(is_stale, no_op_grad, real_grad)
 
-      with ops.device(global_step.device), ops.name_scope(""):
-        # Replicas have to wait until they can get a token from the token queue.
-        with ops.control_dependencies([final_train_ops]):
+      with ops.control_dependencies([final_train_ops]):
+        token = sync_token_queue.dequeue()
+        token = logging_ops.Print(token, [token])
+        with ops.device(global_step.device), ops.name_scope(""):
+          # Replicas have to wait until they can get a token from the token queue.
           # Log start time of worker computation
-          token = sync_token_queue.dequeue()
-          token = logging_ops.Print(token, [token])
           train_op = state_ops.scatter_update(self._local_steps,
                                               self._replica_id,
                                               token, name=name)
