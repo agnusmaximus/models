ba726aa62e8540c3eb9ed07cdd43fb0e8ca26cb0
Max <agnusmaximus@berkeley.edu>
2016-10-03 23:23:37 -0700

    Add collect outputs

---
+++ inception/tools/collect_outputs.sh
@@ -0,0 +1,20 @@
+key_location=../../../DistributedSGD.pem
+
+ips=($(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --region us-west-1 --query "Reservations[*].Instances[*].PublicIpAddress" --output text))
+ips_string=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --region us-west-1 --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+n_hosts=${#ips[@]}
+start=0
+echo "Running machines: ${ips_string}"
+
+index=0
+for ip in ${ips[@]}; do
+    echo $ip
+
+    # Terminate python
+    ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ${key_location} ubuntu@${ip} "bash -s" <<EOF
+pkill python
+EOF
+    # Collect the outputs
+    scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ${key_location} ubuntu@${ip}:~/models/inception/out${index} .
+    index=$((index+1))
+done
