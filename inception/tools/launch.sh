4ae30b1d2a5c1dbf194575e0a4febc89353f50c9
Max <agnusmaximus@berkeley.edu>
2016-10-03 23:05:21 -0700

    Update

--- inception/tools/launch.sh
+++ inception/tools/launch.sh
@@ -6,7 +6,7 @@
 
 # Usage: sh run_distributed.sh [machine_tier] [n_instances]
 
-image_id=ami-f34b0293
+image_id=ami-57470e37
 default_machine_tier='t2.small'
 machine_tier=${1:-$default_machine_tier}
 default_n_instances=5
