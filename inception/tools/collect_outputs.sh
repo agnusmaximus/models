07edfe0b22ea0fa147ca058c40d4441abea147ae
Max <agnusmaximus@berkeley.edu>
2016-10-13 19:11:23 -0700

    Update script

--- inception/tools/collect_outputs.sh
+++ inception/tools/collect_outputs.sh
@@ -1,7 +1,9 @@
 key_location=../../../DistributedSGD.pem
+default_outfile_location="./outfiles"
+outfile_location=${1:$default_outfile_location}
 
-rm -rf ./outfiles
-mkdir outfiles
+rm -rf $(outfile_location)
+mkdir $(outfile_location)
 
 ips=($(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --region us-west-1 --query "Reservations[*].Instances[*].PublicIpAddress" --output text))
 ips_string=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --region us-west-1 --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
@@ -18,6 +20,6 @@ for ip in ${ips[@]}; do
 pkill python
 EOF
     # Collect the outputs
-    scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ${key_location} ubuntu@${ip}:~/models/inception/out${index} ./outfiles
+    scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ${key_location} ubuntu@${ip}:~/models/inception/out${index} $(outfile_location)
     index=$((index+1))
 done
