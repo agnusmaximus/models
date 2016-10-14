e7e02e48a588539731192312065ff8cc019eb412
Max <agnusmaximus@berkeley.edu>
2016-10-14 12:11:16 -0700

    Update

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -21,7 +21,7 @@ from __future__ import division
 from __future__ import print_function
 
 from datetime import datetime
-from sync_replicas_optimizer_summarized import SyncReplicasOptimizerSummarized
+from sync_replicas_optimizer_summarized import SyncReplicasOptimizerSummarized,SyncReplicasOptimizerSummarizedOld
 import os.path
 import time
 
