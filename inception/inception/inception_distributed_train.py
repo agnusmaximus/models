9cd422ecf6c660ef0fc0f79f6d2b910bc65dfd76
Max <agnusmaximus@berkeley.edu>
2016-10-26 22:02:52 -0700

    Update

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -255,12 +255,16 @@ def train(target, dataset, cluster_spec):
       # short circuit from the gradient computations.
       for operation in inception_train_graph.get_operations():
         if "gradients/" in operation.node_def.name:
-          if len(operation.outputs) > 0:
-            short_circuit_op = lambda : [tf.zeros(tf.shape(y), dtype=y.dtype) for y in  operation.outputs]
-            normal_op = lambda : operation.outputs
-            operation = tf.cond(sync_token_queue.size() > 0,
-                                short_circuit_op,
-                                normal_op)
+          #if len(operation.outputs) > 0:
+          #  short_circuit_op = lambda : [tf.zeros(tf.shape(y), dtype=y.dtype) for y in  operation.outputs]
+          #  normal_op = lambda : operation.outputs
+          #  operation = tf.cond(sync_token_queue.size() > 0,
+          #                      short_circuit_op,
+          #                      normal_op)
+          prev_outputs = operations.outputs
+          operation.outputs = tf.cond(sync_token_queue.size() > 0,
+                                      lambda: [tf.zeros(tf.shape(y), dtype=y.dtype) for y in  operation.outputs],
+                                      lambda: prev_outputs)
       tf.logging.info("Injected short circuiting...")
 
       # Build an initialization operation to run below.
