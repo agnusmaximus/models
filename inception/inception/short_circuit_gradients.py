3bc95a287944cf10fdc3e3dfb9781bbcc3c8f781
Max <agnusmaximus@berkeley.edu>
2016-10-29 15:01:05 -0700

    Update

--- inception/inception/short_circuit_gradients.py
+++ inception/inception/short_circuit_gradients.py
@@ -540,7 +540,7 @@ def gradients_short_circuited(ys,
                       [x for x in in_grads if x is not None]) > 1:
                     in_grads = control_flow_ops.tuple(in_grads)
                 _LogOpGradients(op, out_grads, in_grads)
-              return in_grads
+            return in_grads
 
         # If none gradient, no need to do anything
         if not none_gradient:
