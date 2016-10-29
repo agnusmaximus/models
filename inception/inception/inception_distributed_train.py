e4d93015958b2c1c2e2a2f534b4921351825c6be
Max <agnusmaximus@berkeley.edu>
2016-10-28 20:23:11 -0700

    Update

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -284,7 +284,8 @@ def train(target, dataset, cluster_spec):
             cond_short_circuit = tf.identity(tf.Variable(.8), name=name)
             cond_ops = [x for x in inception_train_graph.get_operations() if name in x.name]
 
-            short_circuit_sgv = ge.SubGraphView(cond_ops, passthrough_ts=operation.inputs)
+            #short_circuit_sgv = ge.SubGraphView(cond_ops, passthrough_ts=operation.inputs)
+            short_circuit_sgv = ge.SubGraphView(cond_ops)
 
             print([x for x in short_circuit_sgv.inputs])
             print([x for x in operation.inputs])
