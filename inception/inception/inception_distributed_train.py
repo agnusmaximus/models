19d629efe21249685d4e83b2b5cc10e5212199c1
Max <agnusmaximus@berkeley.edu>
2016-11-03 22:27:11 -0700

    Update

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -124,7 +124,7 @@ def train(target, dataset, cluster_spec):
       # Create a variable to count the number of train() calls. This equals the
       # number of updates applied to the variables.
       global_step = slim.variables.global_step()
-      #local_global_step = tf.Variable(global_step)
+      local_global_step = tf.Variable(global_step)
 
       # Calculate the learning rate schedule.
       num_batches_per_epoch = (dataset.num_examples_per_epoch() /
@@ -302,7 +302,7 @@ def train(target, dataset, cluster_spec):
       next_summary_time = time.time() + FLAGS.save_summaries_secs
       begin_time = time.time()
       while not sv.should_stop():
-        #local_global_step = tf.Variable(global_step.ref())
+        local_global_step = tf.Variable(global_step.ref())
         try:
           start_time = time.time()
           if FLAGS.timeline_logging:
