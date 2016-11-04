a16285add3fa4646f27560edf246795718dbe1f5
Max <agnusmaximus@berkeley.edu>
2016-11-04 02:12:15 -0700

    Update

--- inception/inception/sync_replicas_optimizer_modified.py
+++ inception/inception/sync_replicas_optimizer_modified.py
@@ -352,11 +352,12 @@ class SyncReplicasOptimizerV2(optimizer.Optimizer):
         with ops.control_dependencies([update_op]):
           # Sync_op needs to insert tokens to the token queue at the end of the
           # step so the replicas can fetch them to start the next step.
+          #with ops.control_dependencies([update_local_global_step_op]):
           tokens = array_ops.fill([self._tokens_per_step], global_step.ref())
           sync_op = sync_token_queue.enqueue_many((tokens,))
 
         if self._variable_averages is not None:
-          with ops.control_dependencies([sync_op, update_local_global_step_op]), ops.name_scope(""):
+          with ops.control_dependencies([sync_op]), ops.name_scope(""):
             sync_op = self._variable_averages.apply(
                 self._variables_to_average)
 
