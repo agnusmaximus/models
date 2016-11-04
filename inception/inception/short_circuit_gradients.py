e69defb4828e1aa999b9b912ccd5b32e78c93c60
Max <agnusmaximus@berkeley.edu>
2016-11-04 00:58:42 -0700

    Update

--- inception/inception/short_circuit_gradients.py
+++ inception/inception/short_circuit_gradients.py
@@ -555,7 +555,7 @@ def gradients_short_circuited(ys,
         # If none gradient, no need to do anything
         if not none_gradient:
             new_global_step = tf.identity(global_step)
-            in_grads = tf.cond(new_global_step > local_global_step,
+            in_grads = tf.cond(new_global_step > local_global_step.ref(),
                                zero_grad_function,
                                in_grad_function)
             if type(in_grads) == tf.Tensor:
