185277cda6ee779371ee3f7feec5bff8b5c59302
Max <agnusmaximus@berkeley.edu>
2016-11-04 19:10:41 -0700

    Update

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -220,7 +220,7 @@ def train(target, dataset, cluster_spec):
       # Use V2 optimizer
       opt = SyncReplicasOptimizerV2(
         opt,
-        replicas_to_aggregate=int(num_replicas_to_aggregate * 6.0/7.0),
+        replicas_to_aggregate=int(num_replicas_to_aggregate * 15.0 / 100.0),
         #replicas_to_aggregate=num_replicas_to_aggregate,
         total_num_replicas=num_workers,
         variable_averages=exp_moving_averager,
